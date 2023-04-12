9d00b6e26906c84b0d0c5f30e2e995cce2c1a0f0
Conor Murphy <conormurphyx@gmail.com>
2023-04-12 00:14:03 -0700

    * fix for sudo

--- setup.sh
+++ setup.sh
@@ -1,20 +1,21 @@
+home="$(getent passwd $SUDO_USER | cut -d: -f6)"
 # install vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.vim/plugged
-cp "$1/.vimrc" ~
+curl -fLo $home/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir $home/.vim/plugged
+cp -v "$1/.vimrc" $home
 
 # install zsh
-cp "$1/.zshrc" ~
-apt-get install zsh -y
-chsh -s /bin/zsh
+cp -v "$1/.zshrc" $home
+apt-get install zsh
+sudo -u $SUDO_USER chsh -s /bin/zsh
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
+sudo -u $SUDO_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
 # install powerlevel10k
-cp "$1/.p10k.zsh" ~
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp -v "$1/.p10k.zsh" $home
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$home/.oh-my-zsh/custom}/themes/powerlevel10k
 # install zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$home/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$home/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-$home/.oh-my-zsh}/custom}/plugins/zsh-completions
 
 
