5a6f47b2d1e2edb94c51e34373d9258c6d657e96
conor <conormurphyx@gmail.com>
2023-04-11 21:16:36 -0700

    try to make script quiter

--- setup.sh
+++ setup.sh
@@ -5,16 +5,13 @@ mkdir ~/.vim/plugged
 cp "$1/.vimrc" ./
 
 # install zsh
-sudo apt-get install zsh
-# set zsh to be main bash script
-echo -e "if [ -t 1 ]; then\n  exec zsh\nfi" > .bashrc
-
 cp "$1/.zshrc" ./
+apt-get install zsh -y
+chsh -s /bin/zsh
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-cp "$1/.p10k.zsh" ./
 # install zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
